15c12f7349331851202d69b22f484d4026debf65
cdask <d.ask3@yahoo.com>
2020-05-13 21:19:19 +1000

    TESTING: Wrote validation and association testing for transaction model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:profile) { Profile.new }
+  let(:listing) { Listing.new }
+  subject { described_class.new(
+    listing: listing,
+    profile: profile,
+    rating: 450,
+    review: 'Friendly'
+  )}
+  context 'associations' do
+    it 'belongs to a Listing' do
+      relation = Transaction.reflect_on_association(:listing)
+      expect(relation.macro).to eql(:belongs_to)
+    end
+
+    it 'belongs to a Profile' do
+      relation = Transaction.reflect_on_association(:profile)
+      expect(relation.macro).to eql(:belongs_to)
+    end
+  end
+
+  context 'validations' do
+    it 'is valid with valid attributes' do 
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid with rating below 100' do
+      subject.rating = 50
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with a rating above 500' do
+      subject.rating = 501
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with any number for rating apart from 100, 200, 300, 400 and 500' do
+      subject.rating = 350
+      expect(subject).to_not be_valid
+    end
+  end
 end
