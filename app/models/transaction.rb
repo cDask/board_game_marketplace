593325447c111ad3e790ccfe2adf77715c9c9910
cdask <d.ask3@yahoo.com>
2020-05-13 21:25:38 +1000

    FEATURE: Added validations to transaction model. Passed all tests

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :listing
   belongs_to :profile
   validates :rating, inclusion: {
-    in: [100,200,300,400,500],
+    in: [100, 200, 300, 400, 500],
     message: '%<value> is not a valid rating'
   }
   validates :rating, numericality: { less_than_or_equal_to: 500 }
