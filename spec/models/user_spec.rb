33f2d41ee07a22a36ba6bf29eeb1b3426892da31
cdask <d.ask3@yahoo.com>
2020-05-13 17:04:58 +1000

    TEST: Wrote user model validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(
+    email: 'test@boardgamesexchange.com',
+    password: 'password',
+  )}
+  context 'validations' do
+    it 'is valid with valid attributes' do 
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without an email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with password less than 8 characters' do
+      subject.password = '123456'
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without an admin status' do
+      subject.is_admin = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should default admin status to false' do
+      expect(subject.is_admin).to eq false
+    end
+  end
 end
