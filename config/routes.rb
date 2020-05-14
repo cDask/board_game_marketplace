46168c50e777cba56b0cbf12b0693bfd7e427e3b
cdask <d.ask3@yahoo.com>
2020-05-14 15:21:03 +1000

    FIX: moved rooot routing to see if it fixes heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'listings#index'
-  root to: "listings#index"
+  root "listings#index"
   devise_for :users
   resources :listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
