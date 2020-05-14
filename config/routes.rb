636dfbc96f8fb75fc3263e9dd50a6854bb1dc207
cdask <d.ask3@yahoo.com>
2020-05-14 17:11:43 +1000

    FIX: Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/', to: 'listings#index'
-  root "listings#index"
+  root to: "listings#index"
   devise_for :users
   resources :listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
