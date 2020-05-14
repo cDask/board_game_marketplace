12c2b3bfe51d153ac4d876dcbd0118ebc9ead92e
cdask <d.ask3@yahoo.com>
2020-05-14 10:35:14 +1000

    TESTING: Added association testing for ListingPayments join table

--- spec/models/listing_payment_spec.rb
+++ spec/models/listing_payment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ListingPayment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:listing) { Listing.new }
+  let(:payment) { Payment.new }
+  subject { described_class.new(
+    payment: payment,
+    listing: listing
+  )}
+  context 'associations' do
+    it 'belongs to a Listing' do
+      relation = ListingPayment.reflect_on_association(:listing)
+      expect(relation.macro).to eql(:belongs_to)
+    end
+
+    it 'belongs to a Payment' do
+      relation = ListingPayment.reflect_on_association(:payment)
+      expect(relation.macro).to eql(:belongs_to)
+    end
+  end
 end
