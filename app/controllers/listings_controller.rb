ec3dd61c4a0a73fc5d2da1f065fd94ffbf34366a
cdask <d.ask3@yahoo.com>
2020-05-14 14:34:56 +1000

    FEATURE: Initiated listing controller

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,9 @@
+class ListingsController < ApplicationController
+  def index; end
+
+  def show; end
+
+  def new; end
+
+  def edit; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root to: "users#index"
+  root to: "listings#index"
 end
