4fbd91d1d1cbee45b40fe12f7b5749a45945b9c0
cdask <d.ask3@yahoo.com>
2020-05-14 14:15:43 +1000

    TESTING: Added validation and association testing for message model

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:profile) { Profile.new }
+  let(:conversation) { Conversation.new }
+  subject { described_class.new(
+    profile: profile,
+    conversation: conversation,
+    body: 'Hello World'
+  )}
+  context 'associations' do
+    it 'belongs to a conversation' do
+      relation = Messsage.reflect_on_association(:conversation)
+      expect(relation.macro).to eql(:belongs_to)
+    end
+
+    it 'belongs to an profile' do
+      relation = Message.reflect_on_association(:profile)
+      expect(relation.macro).to eql(:belongs_to)
+    end
+  end
+
+  context 'validations' do
+    it 'is valid with valid attributes' do 
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a message body' do
+      subject.body = nil
+      expect(subject).t_not be_valid
+    end
+  end
 end
