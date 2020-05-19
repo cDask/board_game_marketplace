e929302ce19aa2d00c59f9a8e7ed51f8a692b2d5
cdask <d.ask3@yahoo.com>
2020-05-19 15:42:10 +1000

    FIX: fixed peopler with profile being able to create new ones

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,17 +5,20 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    user.profile ||= Profile.new
+    profile = user.profile
+    profile ||= Profile.new
     can :manage, :all if user.is_admin?
     can %i[index show new create], Listing
-    can %i[edit update destroy], Listing, profile: user.profile
+    can %i[edit update destroy], Listing, profile: profile
     profile_permissions(user)
-    can :manage, Conversation, author_id: user.profile.id
-    can :manage, Conversation, receiver_id: user.profile.id
+    can :manage, Conversation, author_id: profile.id
+    can :manage, Conversation, receiver_id: profile.id
   end
 
   def profile_permissions(user)
-    can %i[new create], Profile
+    can %i[new create], Profile do
+      user.profile.nil?
+    end
     can %i[index show edit], Profile, user_id: user.id
   end
 end
